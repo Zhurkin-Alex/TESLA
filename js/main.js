8f029bd756cf801d944ac51304c1cc151299db7e
Aleksey <lexus1812141@gmail.com>
2020-04-05 17:13:55 +0300

    изменения

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ $('.closed-btn').on('click', function(){
 });
 });
 
+// $(document).ready(function() {
+
+// 	$("#modal").submit(function() {
+// 		$.ajax({
+// 			type: "POST",
+// 			url: "/mail.php",
+// 			data: $(this).serialize()
+// 		}).done(function() {
+// 			$(this).find("input").val("");
+// 			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+// 			$("#form").trigger("reset");
+// 		});
+// 		return false;
+// 	});
+	
+// });
